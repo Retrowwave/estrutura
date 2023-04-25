0f376da7ebcdaf65bbc1035f4e67da7d59c8e1c7
Retrowwave <alexkeppler@hotmail.com>
2023-04-24 22:22:21 -0300

    teste

--- src/App.java
+++ src/App.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 // Neste exemplo estamos criando uma estrutura de seleção múltipla, podendo escolher
-// entre as opções 1, 2 e 3.
+// Entre as opções 1, 2 e 3.
 // Caso seja escolhido qualquer coisa que não seja alguma dessas opções, iré retornar
-// a seguinte mensagem: Selecione alguma opção entre 1 até 3:
+// A seguinte mensagem: Selecione alguma opção entre 1 até 3:
+// Teste
 
     Scanner scanner = new Scanner(System.in);
     System.out.println("Digite uma opção:");
     System.out.println("|1|2|3|");
     int opcao = scanner.nextInt();
+
+    System.out.println("teste");
 
     switch (opcao) {
         case 1:
